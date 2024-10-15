602954249076490d37d3fd2551e2ecd012261355
Abhilash <abhilash.panicker@outlook.com>
2024-10-15 10:28:25 +0530

    Update ContactUs.jsx

--- pages/ContactUs.jsx
+++ pages/ContactUs.jsx
@@ -8,6 +8,8 @@ import Link from 'next/link';
 import Image from 'next/image';
 import {supabase} from './api/sendEmail'
 
+
+
 const ContactUs = () => {
   const [formData, setFormData] = useState({
     name: '',
