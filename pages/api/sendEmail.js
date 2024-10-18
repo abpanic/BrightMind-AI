e5e52e33074059b2e8863d741f39d8d58356d6b6
Abhilash <abhilash.panicker@outlook.com>
2024-10-18 21:24:45 +0530

    updating basic components

--- pages/api/sendEmail.js
+++ pages/api/sendEmail.js
@@ -1,28 +1,15 @@
 import Mailjet from 'node-mailjet';
-import { createClient } from '@supabase/supabase-js';
-
-
-MJ_APIKEY_PUBLIC='d5d3f2980d2add4f38836c69450462f3'
-MJ_APIKEY_PRIVATE='87cbb3fafd1faf45a69bece6592d039b'
-
-
-NEXT_PUBLIC_SUPABASE_URL='https://sobhmltowefsfutqwncb.supabase.co'
-NEXT_PUBLIC_SUPABASE_ANON_KEY='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNvYmhtbHRvd2Vmc2Z1dHF3bmNiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjg1MDQ1NjcsImV4cCI6MjA0NDA4MDU2N30.DQoVOdEWn9UX0FH8Ye1BFS0ofCNQOv19yPYqjHP1QE8'
-
-// Initialize Supabase client
-export const supabase = createClient(NEXT_PUBLIC_SUPABASE_URL, NEXT_PUBLIC_SUPABASE_ANON_KEY);
 
 const mailjet = new Mailjet({
-  apiKey: MJ_APIKEY_PUBLIC,
-  apiSecret: MJ_APIKEY_PRIVATE,
+  apiKey: process.env.MJ_APIKEY_PUBLIC,
+  apiSecret: process.env.MJ_APIKEY_PRIVATE,
 });
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     const { name, email, phone, careerPath, message, couponCode, workExperience } = req.body;
 
-    let emailSuccess = false;
-    let supabaseSuccess = false;
+    let emailSuccess = false;    
 
     try {
       // Send Email via Mailjet
@@ -74,30 +61,8 @@ export default async function handler(req, res) {
       console.error('Failed to send email: ', error);
     }
 
-    try {
-      // Write contact form data to Supabase
-      const { data, error } = await supabase
-        .from('contact_forms')
-        .insert([{ 
-          name, 
-          email, 
-          phone, 
-          career_path: careerPath, 
-          couponCode, 
-          message, 
-          work_experience: workExperience 
-        }]);
-
-      if (error) {
-        throw new Error('Error saving data to Supabase: ' + error.message);
-      }
-      supabaseSuccess = true;
-    } catch (error) {
-      console.error('Failed to save to Supabase: ', error);
-    }
-
-    // Return success status for both email and supabase actions
-    res.status(200).json({ emailSuccess, supabaseSuccess });
+       // Return success status for both email and supabase actions
+    res.status(200).json({ emailSuccess});
   } else {
     res.setHeader('Allow', ['POST']);
     res.status(405).end(`Method ${req.method} Not Allowed`);
